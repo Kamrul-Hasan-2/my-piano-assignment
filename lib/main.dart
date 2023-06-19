801fe9b44d31b71bc7389bbe8277207c558dd007
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2023-06-20 00:22:16 +0600

    Piano 🎹

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MyPianoApp());
 
 class MyPianoApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({Color? color, int? soundNumber}) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: color),
+        child: null,
+        onPressed: () {
+          playSound(soundNumber!);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+
             children: <Widget>[
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                child: null,
-                onPressed: () {
-                  //playSound();
-                },
-              )
+              buildKey(color: Colors.red, soundNumber:1),
+              buildKey(color: Colors.orange, soundNumber:2),
+              buildKey(color: Colors.pink, soundNumber:3),
+              buildKey(color: Colors.teal, soundNumber:4),
+              buildKey(color: Colors.green, soundNumber:5),
+              buildKey(color: Colors.purpleAccent, soundNumber:6),
+              buildKey(color: Colors.yellow, soundNumber:7),
             ],
           ),
         ),
